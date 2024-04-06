4554008855cf2da940c50a60452d01a5aa5ac38d
imamogluomer <imamogluomer@outlook.com>
2024-04-06 23:13:07 +0300

    Final version of the app.

---
+++ app/src/main/java/com/omerimamoglu/counterapp/CounterModel.kt
@@ -0,0 +1,23 @@
+package com.omerimamoglu.counterapp
+
+data class CounterModel(var count : Int)
+
+class CounterRepository{
+    private var counter = CounterModel(0)
+
+    fun getCounter() = counter
+
+    fun incrementCounter(){
+        counter.count++
+
+    }
+    fun decrementCounter(){
+        counter.count--
+
+    }
+
+
+
+
+
+}
